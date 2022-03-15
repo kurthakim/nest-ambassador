532221fd0291264e672657366192f9188ec2df02
Kurt Hakim <kurthakim@gmail.com>
2022-03-15 15:04:42 +0700

    added mailer but commented

--- src/order/listeners/order.listener.ts
+++ src/order/listeners/order.listener.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { Order } from '../order';
 import { client } from '../../main';
+// import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class OrderListener {
-  constructor() {}
+  constructor() {} // private mailerService: MailerService
 
   @OnEvent('order_completed')
   async handleOrderCompletedEvent(order: Order) {
     client.zIncrBy('rankings', order.ambassador_revenue, order.name);
+
+    await this.mailerService.sendMail({
+      to: 'admin@admin.com',
+      subject: 'An order has been completed',
+      html: `Order #${order.id} with a total of ${order.total} has been completed!`,
+    });
+
+    await this.mailerService.sendMail({
+      to: order.ambassador_email,
+      subject: 'An order has been completed',
+      html: `You earned #${order.ambassador_revenue} from the link #${order.code}`,
+    });
   }
 }
